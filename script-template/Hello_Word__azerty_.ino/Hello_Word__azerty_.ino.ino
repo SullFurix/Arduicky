42f7c2de252c9e1074b840c48525a5f55adb8053
SullFurix <39709633+SullFurix@users.noreply.github.com>
2019-06-13 21:37:55 +0200

    1.0.1 General Availability

---
+++ script-template/Hello_Word__azerty_.ino/Hello_Word__azerty_.ino.ino
@@ -0,0 +1,21 @@
+//Windows files General Availability by SullFurix: 12/06/2019
+#include <HIDKeyboard.h>
+
+HIDKeyboard keyboard; 
+
+  void setup() {
+    keyboard.begin();
+  } 
+
+  void loop() {
+    keyboard.pressKey(GUI, 'r'); // Press Windows+R
+    delay(2000); // Wait 2 secondes
+    keyboard.println("notepqd"); // Types 'notepad'
+    keyboard.releaseKey(); // Releases keys
+    delay(2000); // Wait 2 secondes
+    keyboard.pressSpecialKey(ENTER); // Sends an "Enter" keypress
+    keyboard.releaseKey(); // Releases "Enter"
+    delay(2000); // Wait 2 secondes
+    keyboard.println("Hello Zord"); // Types "Hello World!" followed by a carriage return
+    while(1); // Hold forever
+  }
